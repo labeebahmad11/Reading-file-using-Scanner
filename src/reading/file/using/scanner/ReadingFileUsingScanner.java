a645a1e16a196a3cc82773fe94dd226141bad92e
labeebahmad11 <labeeb.ahmad@hotmail.com>
2015-02-24 17:16:35 +0500

    Program files

---
+++ src/reading/file/using/scanner/ReadingFileUsingScanner.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package reading.file.using.scanner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Home
+ */
+public class ReadingFileUsingScanner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException 
+    {
+        
+        File file = new File("C:\\Users\\Home\\Desktop\\xanadu.txt");
+        Scanner s = new Scanner(file);
+        
+        while (s.hasNextLine()) 
+        {            
+            System.out.println(s.nextLine());
+        }
+        
+        
+    }
+    
+}
